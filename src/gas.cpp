7ac045f6d261ec6deb3e82eab588596078593d8e
aeventyr <124109819+aeventyr@users.noreply.github.com>
2024-11-11 16:25:46 -0800

    Update gas.cpp
    show new formula for low total health multiplier

--- src/gas.cpp
+++ src/gas.cpp
@@ -10,7 +10,7 @@ unsigned int GAS::gaugeCycleIdx = 0;
 
 void GAS::Init()
 {
-	gaugeArr.clear();
+    gaugeArr.clear();
     gaugeArr.emplace_back(Gauge(GAUGE_P_ATTACK));
     gaugeArr.emplace_back(Gauge(GAUGE_HAZARD));
     gaugeArr.emplace_back(Gauge(GAUGE_HARD));
@@ -32,25 +32,26 @@ Gauge& GAS::PickDisplay()
 
 double GAS::GetHealthMultiplier()
 {
-	double tNotes;
-	// low notes multiplier
-	if (notesNum < 20)
-		tNotes = 10.0;
-	else if (notesNum < 30)
-		tNotes = 14.0 - (notesNum / 5.0);
-	else if (notesNum < 60)
-		tNotes = 9.0 - (notesNum / 15.0);
-	else if (notesNum < 125)
-		tNotes = 5.0 - ((notesNum - 60.0) / 65.0);
-	else if (notesNum < 250)
-		tNotes = 5.0 - (notesNum / 125.0);
-	else if (notesNum < 500)
-		tNotes = 4.0 - (notesNum / 250.0);
-	else if (notesNum < 1000)
-		tNotes = 3.0 - (notesNum / 500.0);
-	else
-		tNotes = 1.0;
-	// low total multiplier
-	double tTotal = (bmsTotal <= 80) ? (10) : (160.0 / (bmsTotal - 80));
-	return std::max(tTotal, tNotes);
+    double tNotes;
+    // low notes multiplier
+    if (notesNum < 20)
+        tNotes = 10.0;
+    else if (notesNum < 30)
+        tNotes = 14.0 - (notesNum / 5.0);
+    else if (notesNum < 60)
+        tNotes = 9.0 - (notesNum / 15.0);
+    else if (notesNum < 125)
+        tNotes = 5.0 - ((notesNum - 60.0) / 65.0);
+    else if (notesNum < 250)
+        tNotes = 5.0 - (notesNum / 125.0);
+    else if (notesNum < 500)
+        tNotes = 4.0 - (notesNum / 250.0);
+    else if (notesNum < 1000)
+        tNotes = 3.0 - (notesNum / 500.0);
+    else
+        tNotes = 1.0;
+    // low total multiplier
+    int tTotalInv = std::min(10, std::max(1, int(bmsTotal - 80) / 16));
+    double tTotal = 10.0 / tTotalInv;
+    return std::max(tTotal, tNotes);
 }
